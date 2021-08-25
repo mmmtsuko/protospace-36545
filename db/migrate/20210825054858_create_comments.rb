f24e224ef8f09240ab9a583ac22952c7fe43be3e
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-08-25 23:54:50 +0900

    コメントコントローラ再設定

--- db/migrate/20210825054858_create_comments.rb
+++ db/migrate/20210825054858_create_comments.rb
@@ -1,10 +1,10 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-       t.text :text     
-       #t.references :user, null: false,     foreign_key: true
-       #t.references :protoype, null: false, foreign_key: true
-       t.timestamps
+        t.text :text     
+        t.references :user, null: false,     foreign_key: true
+        #t.references :protoype, null: false, foreign_key: true
+        t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,10 @@ ActiveRecord::Schema.define(version: 2021_08_25_101554) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "prototypes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -39,4 +41,5 @@ ActiveRecord::Schema.define(version: 2021_08_25_101554) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "users"
 end
