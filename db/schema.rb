004e3fd9654feae28269f63282e6fcfc0935d623
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-08-26 17:01:26 +0900

    prototypesのマイグレーションファイル設定

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,13 @@ ActiveRecord::Schema.define(version: 2021_08_25_101554) do
   end
 
   create_table "prototypes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "catch_copy", null: false
+    t.text "concept", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -42,4 +47,5 @@ ActiveRecord::Schema.define(version: 2021_08_25_101554) do
   end
 
   add_foreign_key "comments", "users"
+  add_foreign_key "prototypes", "users"
 end
