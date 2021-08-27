8a227b2f1224a2a59c0d8e535e72491a477911f2
渡邊光子 <88422268+mmmtsuko@users.noreply.github.com>
2021-08-27 11:13:08 +0900

    createアクションに、データが保存されなかったときは詳細ページに戻るようrenderを用いて記述し

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,18 @@
 class CommentsController < ApplicationController
   def create
-    Comment.create(comment_params)
-    redirect_to "/tweets/#{comment.tweet.id}" 
- end
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to prototype_path(@comment.prototype)
+    else
+      @prototype = @comment.prototype
+      @comments = @prototype.comments
+      render "prototypes/show"
+    end
+  end
 
   private
 
- def comment_params
-  paramas.require(:comment).permit(:text).merge(user_id: current_user.id: params[:tweet_id])
- end
-
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
+  end
 end
